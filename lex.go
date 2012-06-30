a2ca384211c53092b3ecb84ec81b2e698a2b77de
Pietro Gagliardi <pietro10@mac.com>
2012-06-30 11:10:06 -0400

    Finished first version of lexer.

--- lex.go
+++ lex.go
@@ -3,7 +3,8 @@ package main
 
 import (
 	// FileLexer
-	"utf8"
+	"unicode"
+	"unicode/utf8"
 	"bufio"
 
 	// Lexer
@@ -17,7 +18,36 @@ var nErrors uint64
 const (
 	lexEOF rune = utf8.MaxRune + 1 + iota
 	lexError
+	lexNoC			// for continuations
 )
+
+var isContinuationToken = map[int]bool{
+	lexNoC:	true,
+	OR:		true,
+	AND:	true,
+	EQ:		true,
+	NE:		true,
+	'<':		true,
+	LE:		true,
+	'>':		true,
+	GE:		true,
+	'+':		true,
+	'-':		true,
+	'|':		true,
+	'^':		true,
+	'*':		true,
+	'/':		true,
+	'%':		true,
+	'&':		true,
+	LSH:		true,
+	RSH:		true,
+	'~':		true,
+	'!':		true,
+	'(':		true,
+	'{':		true,			// TODO this will be used later
+	':':		true,
+	// TODO add TERM? will this save time?
+}
 
 type FileLexer struct {
 	Filename	string
@@ -26,7 +56,7 @@ type FileLexer struct {
 	Tokens	chan yySymType
 	inputLine	string
 	tokStart	uint64
-	tokEnd	uint64
+	readPos	uint64		// also the position where the token ends
 	runeLen	uint64		// for ignoring the current character
 	lastTok	int			// for automatically inserting ::
 }
@@ -49,15 +79,26 @@ func (l *FileLexer) Error(e string) {
 func (l *FileLexer) Emit(toktype int) {
 	l.Tokens <- yySymType{
 		type:		toktype,
-		value:	line[l.tokStart:l.tokEnd],
+		value:	line[l.tokStart:l.readPos],
 	}
 	l.lastTok = toktype
-	l.tokStart = l.tokEnd		// advance
+	l.tokStart = l.readPos		// advance
+}
+
+func (l *FileLexer) getline() error {
+	line, err := l.File.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	l.inputLine = line
+	l.tokStart = 0
+	l.tokEnd = 0
+	l.runeLen = 0
+	return nil
 }
 
 func (l *FileLexer) read() (rune, error) {
-	l.advance()
-	if l.tokStart >= len(l.inputLine) {
+	if l.readPos >= len(l.inputLine) {
 		err := l.getline()
 		if err == io.EOF {
 			l.runeLen = 0			// don't unget an EOF
@@ -67,18 +108,19 @@ func (l *FileLexer) read() (rune, error) {
 			return lexError, err		// TODO more proper return?
 		}
 	}
-	r, l.runeLen := utf8.DecodeRuneInString(len[i.tokStart:])
-	l.tokEnd = l.tokStart + l.runeLen
+	r, l.runeLen := utf8.DecodeRuneInString(len[i.readPos:])
+	l.readPos += l.runeLen
+	// tokStart is updated either when we emit a token or when we ignore one
 	return r, nil
 }
 
-func (l *FileLexer) advance() {
-	l.tokStart = l.tokEnd
+func (l *FileLexer) ignore() {
+	l.tokStart = l.readPos
 	l.runeLen = 0
 }
 
 func (l *FileLexer) unget() {
-	l.tokEnd -= l.runeLen
+	l.readPos -= l.runeLen
 }
 
 func (l *FileLexer) peek() (r rune, err error) {
@@ -91,15 +133,205 @@ func (l *FileLexer) peek() (r rune, err error) {
 	return
 }
 
-func (l *FileLexer) getline() error {
-	line, err := l.File.ReadString('\n')
-	if err != nil {
-		return err
+func (l *FileLexer) accept(r rune) bool {
+	c, err := l.read()
+	if err != nil { select { } }	// TODO
+	if r != c {
+		l.unget()
+		return false
 	}
-	l.inputLine = line
-	l.tokStart = 0
-	l.tokEnd = 0
-	l.runeLen = 0
+	return true
+}
+
+func (l *FileLexer) acceptAndEmit(r rune, ifSo int, ifNot int) {
+	if l.accept(r) {
+		emit(ifSo)
+	} else {
+		emit(ifNot)
+	}
+}
+
+func lex_next(l *FileLexer) lexState {
+	c, err := l.read()
+	if err != nil { select { } }	// TODO
+	switch {
+	case c == lexEOF:
+		return lex_end
+	case c == '\n':
+		l.ignore()
+		l.Lineno++
+		if !isContinuationToken[l.lastTok] {		// add a terminator unless it's nonsensical to do so
+			l.emit(TERM)
+			l.lastTok = lexNoC
+		}
+		return lex_next
+	case ';':				// comment; eat line
+		for {
+			c, err = l.read()
+			if err != nil { select { } }			// TODO
+			if c == '\n' {
+				l.unget()
+				break
+			}
+			l.ignore()
+		}
+		return lex_next
+	case unicode.IsSpace(c);
+		l.ignore()
+		return lex_next
+	case '0' <= c && c <= '9':
+		l.unget()
+		return lex_decimalNumber
+	case '%':
+		// keep the % in the input; we read it out in the expression evaluation step
+		return lex_binaryNumber
+	case '$':
+		// keep the $ in the input; we read it out in the expression evaluation step
+		return lex_hexNumber
+	case unicode.IsLetter(c) || c == '_' || c == '.':
+		l.unget()
+		return hex_ident
+	case '\'':
+		l.ignore()
+		return lex_character
+	case '"':
+		l.ignore()
+		return lex_string
+	case ':':					// : or :: (terminator)
+		l.acceptAndEmit(':', TERM, ':')
+	case '&':					// & or &&
+		l.acceptAndEmit('&', AND, '&')
+	case '|':					// | or ||
+		l.acceptAndEmit('|', OR, '|')
+	case '=':					// = or ==
+		l.acceptAndEmit('=', EQ, '=')
+	case '!':					// ! or !=
+		l.acceptAndEmit('=', NE, '!')
+	case '<':					// < or <= or <<
+		if l.accept('=') {
+			l.emit(LE)
+		} else if l.accept('<') {
+			l.emit(LSH)
+		} else {
+			l.emit('<')
+		}
+	case '>':					// > or >= or >>
+		if l.accept('=') {
+			l.emit(GE)
+		} else if l.accept('>') {
+			l.emit(RSH)
+		} else {
+			l.emit('>')
+		}
+	// TODO more multi-character tokens
+	default:
+		l.emit(c)
+	}
+	return lex_next			// TODO tail call optimize?
+}
+
+func lex_decimalNumber(l *FileLexer) lexState {
+	l.acceptRun("0123456789")
+	emit(NUMBER)
+	return lex_next
+}
+
+func lex_binaryNumber(l *FileLexer) lexState {
+	l.acceptRun("01")
+	emit(NUMBER)
+	return lex_next
+}
+
+func lex_hexNumber(l *FileLexer) lexState {
+	l.acceptRun("0123456789ABCDEFabcdef")
+	emit(NUMBER)
+	return lex_next
+}
+
+func lex_ident(l *FileLexer) lexState {
+	for {
+		c, err := l.read()
+		if err != nil { select { } }	// TODO
+		if !unicode.IsLetter(c) && c != '_' && c != '.' {
+			if c < '0' || c > '9' {
+				break
+			}
+		}
+	}
+	l.unget()
+	// TODO look up in symbol table
+	emit(IDENT)
+	return lex_next
+}
+
+func getStringCharacter(l *FileLexer) (r rune, isEscaped bool) {
+	var err error
+
+	r, err = l.read()
+	if err != nil { select { } }			// TODO
+	if r == '\\' {					// TODO have things like \u?
+		isEscaped = true
+		r, err = l.read()
+		if err != nil { select { } }		// TODO
+		switch r {
+		case 'n':
+			r = '\n'
+		// TODO more combinations
+		}
+		// default is to take that character literally
+	}
+	return
+
+func hex_character(l *FileLexer) lexState {
+	var count uint64
+
+	// TODO worry about the length
+	// TODO worry about allowing Unicode (meaning we have to worry about character encodings and ugh; probably best to worry about the length later
+	for {
+		r, isExcaped := getStringCharacter(l)
+		if r == lexEOF {
+			l.Error("EOF in character literal")
+			return lex_next		// TODO
+		}
+		count++
+		if !isEscaped && r == '\'' {
+			l.unget()
+			count--
+			break
+		}
+	}
+	if count == 0 {
+		l.Error("empty character literal")
+		return lex_next			// TODO
+	} else if count > 4 {
+		l.Error("character literal too long (max 4 characters)")
+		return lex_next			// TODO
+	}
+	emit(CHARACTER)
+	l.read(); l.ignore()				// TODO handle closing '
+	return lex_next
+}
+
+func hex_string(l *FileLexer) lexState {
+	for {
+		r, isExcaped := getStringCharacter(l)
+		if r == lexEOF {
+			l.Error("EOF in string literal")
+			return lex_next		// TODO
+		}
+		if !isEscaped && r == '\"' {
+			l.unget()
+			break
+		}
+	}
+	// TODO should we handle empty strings?
+	emit(STRING)
+	l.read(); l.ignore()				// TODO handle closing "
+	return lex_next
+}
+
+func lex_end(l *FileLexer) lexState {
+	l.emit(-1)				// EOF
 	return nil
 }
 
