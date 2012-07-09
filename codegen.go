d0cd1d5586fb91a272fbae88a2df43df30b708ac
Pietro Gagliardi <pietro10@mac.com>
2012-07-08 23:46:21 -0400

    Added most of the integer opcodes beginning with a

--- codegen.go
+++ codegen.go
@@ -86,7 +86,7 @@ func o_adda(suffix rune, src Operand, dest Operand) error {
 
 // addi #xxx,<ea>
 func o_addi(suffix rune, src Operand, dest Operand) error {
-	sizes := map[rune][2]byte {
+	sizes := map[rune][2]byte{
 		'b':	{ 0, 0 },
 		'w':	{ 0, 1 },
 		'l':	{ 1, 0 },
@@ -102,3 +102,96 @@ func o_addi(suffix rune, src Operand, dest Operand) error {
 	}
 	return nil
 }
+
+// addq #xxx,<ea>
+func o_addq(suffix rune, src Operand, dest Operand) error {
+	sizes := map[rune][2]byte{
+		'b':	{ 0, 0 },
+		'w':	{ 0, 1 },
+		'l':	{ 1, 0 },
+	}
+
+	if dest.Type == 'a' && suffix == 'b' {		// no byte writes to address registers
+		// TODO print the register?
+		return fmt.Errorf("addq.b cannot be used with an address register destination")
+	}
+	// MAJOR TODO
+	if !src.Expr.CanEvaluateNow() {
+		return fmt.Errorf("sorry, technical restrictions require arguments to addq be evaluatable at code generation time; this will be fixed later")
+	}
+	n := src.Expr.CanEvaluateNow()
+	if n > 8 {
+		return fmt.Errorf("addq immediate argument must be in the range 0 <= n <= 8; received $%X", n)
+	}
+	if n == 8 {
+		n = 0
+	}
+	WriteBits(0, 1, 0, 1)
+	WriteRegNum(byte(n))	// just reuse this because it does what we want
+	WriteBits(0)
+	WriteBits(sizes[suffix]...)
+	WriteEANow(dest)
+	return nil
+}
+
+// addx dN,dN
+// addx -(aN),-(aN)
+func o_addx(suffix rune, src Operand, dest Operand) error {
+	sizes := map[rune][2]byte{
+		'b':	{ 0, 0 },
+		'w':	{ 0, 1 },
+		'l':	{ 1, 0 },
+	}
+
+	if src.Type != dest.Type {
+		return fmt.Errorf("addx operand types must be the same (either both dN or both -(aN))")
+	}
+	WriteBits(1, 1, 0, 1)
+	WriteRegNum(dest.Reg)
+	WriteBits(1)
+	WriteBits(sizes[suffix]...)
+	WriteBits(0, 0)
+	if src.Type == 'd' {		// R/M bit; dN is R
+		WriteBits(0)
+	} else {				// -(aN) is M
+		WriteBits(1)
+	}
+	WriteRegNum(src.Reg)
+	return nil
+}
+
+// and <ea>,dN
+// and dN,<ea>
+func o_and(suffix rune, src Operand, dest Operand) error {
+	opmodes := Opmodes{
+		{	// add <ea>,dN
+			'b':	{ 0, 0, 0 },
+			'w':	{ 0, 0, 1 },
+			'l':	{ 0, 1, 0 },
+		}, {	// add dN,<ea>
+			'b':	{ 1, 0, 0 },
+			'w':	{ 1, 0, 1 },
+			'l':	{ 1, 1, 0 },
+		},
+	}
+
+	if src.Type != 'd' && dest.Type != 'd' {		// at least one operand must be a data register
+		// TODO print more information?
+		return fmt.Errorf("at least one operand of and must be a data register")
+	}
+	WriteBits(1, 1, 0, 0)
+	if dest.Type == 'd' {		// add <ea>,dN
+		WriteRegNum(dest.Reg)
+		WriteBits(opmodes[0][suffix]...)
+		WriteEANow(src)
+	} else {				// add dN,<ea>
+		WriteRegNum(src.Reg)
+		WriteBits(opmodes[1][suffix]...)
+		WriteEANow(dest)
+	}
+	return nil
+}
+
+// TODO andi
+
+// TODO asl/asr
