ab44fc8894e06ff55908f129e63bf85ea7661974
Pietro Gagliardi <pietro10@mac.com>
2012-07-28 11:16:33 -0400

    Added lots of stuff to the debugging output. Made the lexer instance global to suit.

--- debug.go
+++ debug.go
@@ -1,3 +1,4 @@
+
 // 29 june 2012
 // this file contains stuff useful to developing a project as fault-critical as an assembler
 package main
@@ -6,18 +7,78 @@ import (
 	"fmt"
 	"os"
 	"runtime/debug"
+	"encoding/hex"
 	"log"
 )
 
 func _bug(mode string, format string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, "==========\n")
-	fmt.Fprintf(os.Stderr, "!! %s !! ", mode)
-	fmt.Fprintf(os.Stderr, format, args...)
-	fmt.Fprintf(os.Stderr, "\nreport this to pietro/andlabs, including the following:\n")
-	debug.PrintStack()
+	out := os.Stderr
+
+	fmt.Fprintf(out, "==========\n")
+	fmt.Fprintf(out, "!! %s !! ", mode)
+	fmt.Fprintf(out, format, args...)
+	fmt.Fprintf(out, "\nreport this to pietro/andlabs, including the following:\n")
+
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, "Stack trace: ")
+	debug.PrintStack()	// TODO write to out
+
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, "Lexer state: ")
+	if len(AsmLexer.files) == 0 {
+		fmt.Fprintln(out, "no open files")
+	} else {
+		found := false
+		for i := 0; i < len(AsmLexer.files); i++ {
+			if AsmLexer.files[i] == AsmLexer.curfile {
+				fmt.Fprintf(out, "> ")
+				found = true
+			} else {
+				fmt.Fprintf(out, "  ")
+			}
+			fmt.Fprintf(out, "%#v\n", AsmLexer.files[i])
+		}
+		if !found {
+			fmt.Fprintln(out, "...huh, somehow the current file isn't one of the above?")
+			fmt.Fprintf(out, "current file: %#v\n", AsmLexer.curfile)
+		}
+	}
+
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, "Global symbols: ")
+	if len(Symbols.m) == 0 {
+		fmt.Fprintln(out, "none")
+	} else {
+		for k, v := range Symbols.m {
+			fmt.Fprintf(out, "%s :: %s\n", k, v.String())
+		}
+	}
+
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, "Local symbols: ")
+	if len(Locals.m) == 0 {
+		fmt.Fprintln(out, "none")
+	} else {
+		for k, v := range Locals.m {
+			fmt.Fprintf(out, "%s :: %s\n", k, v.String())
+		}
+	}
+
+	// TODO dump later expressions
+
 	// TODO dump more state?
+
+	// tristanseifert suggested making this last because I couldn't decide whether or not it should be first or last
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, "Present assembled data: ")
+	if Output.Len() > 0 {
+		fmt.Fprintln(out, hex.Dump(Output.Bytes()))
+	} else {
+		fmt.Fprintln(out, "no output")
+	}
+
 	// TODO quit?
-	fmt.Fprintf(os.Stderr, "==========\n")
+	fmt.Fprintf(out, "==========\n")
 }
 
 func POTENTIAL_BUG(format string, args ...interface{}) {
--- main.go
+++ main.go
@@ -6,15 +6,19 @@ import (
 	"log"
 )
 
-func main() {
-	var l Lexer
+var AsmLexer *Lexer
 
+func init() {
+	AsmLexer = new(Lexer)
+}
+
+func main() {
 	for i := len(os.Args) - 1; i > 0; i-- {
-		err := l.Open(os.Args[i])
+		err := AsmLexer.Open(os.Args[i])
 		if err != nil {
 			log.Fatalf("error opening %s: %v", os.Args[i], err)
 		}
 	}
 	yyDebug = 100
-	yyParse(&l)
+	yyParse(AsmLexer)
 }
