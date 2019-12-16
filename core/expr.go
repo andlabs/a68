58488dc5f2610ff65dc3d9fc15ac713dc3f01e3a
Pietro Gagliardi <pietro10@mac.com>
2019-12-16 02:51:34 -0500

    Restructured expr.go to be cleaner; finished the writing interfaces.

--- core/expr.go
+++ core/expr.go
@@ -2,7 +2,9 @@
 package core
 
 import (
+	"io"
 	"encoding/binary"
+	"bytes"
 )
 
 // TODO properly mark internal errors?
@@ -74,84 +76,88 @@ func (e ExprOpcode) String() string {
 
 type exprOp struct {
 	code		ExprOpcode
-	int		uint64
-	str		string
+	int		uint64		// (ExprInt, ExprName - see below comment)
+	str		string		// (ExprName) length is stored in int to simplify below code
 }
 
-func (e *exprOp) MarshalBinary() ([]byte, error) {
-	bcode := byte(e.code)
-	if e.code == ExprInt {
-		b := make([]byte, 1 + binary.MaxVarintLen64)
-		b[0] = bcode
-		n := binary.PutUvarint(b[1:], e.int)
-		return b[:1 + n], nil
+// TODO allow overriding what's returned on EOF
+func readError(err error) error {
+	if err == io.EOF {
+		err = io.ErrUnexpectedEOF
+	}
+	return err
+}
+
+// binary.ReadUvarint() requires an io.ByteReader, grrr
+type fakeByteReader struct {
+	r	io.Reader
+}
+func (r *fakeByteReader) ReadByte() (byte, error) {
+	b := make([]byte, 0)
+	_, err := io.ReadFull(r.r, b)
+	if err != nil {
+		return 0, err
+	}
+	return b[0], nil
+}
+func toByteReader(r io.Reader) io.ByteReader {
+	if br, ok := r.(io.ByteReader); ok {
+		return br
+	}
+	return &fakeByteReader{r}
+}
+
+// TODO convert this into a ReadFrom
+func readExprOp(r io.Reader) (e exprOp, err error) {
+	br := toByteReader(r)
+	b, err := br.ReadByte()
+	if err != nil {
+		return exprOp{}, readError(err)
+	}
+	e.code = exprOpcode(b)
+	if e.code >= nExprOpcodes {
+		return exprOp{}, fmt.Errorf("bad opcode 0x%X", code)
+	}
+	if e.code == ExprInt || e.code == ExprName {
+		e.int, err = binary.ReadUvarint(br)
+		if err != nil {
+			return exprOp{}, readError(err)
+		}
 	}
 	if e.code == ExprName {
-		l := len(e.str)
-		b := make([]byte, 1 + binary.MaxVarintLen64 + l)
-		b[0] = bcode
-		n := binary.PutUvarint(b[1:], uint64(l))
-		copy(b[1 + n:], e.str)
-		return b[:1 + n + l], nil
+		buf := make([]byte, e.int)
+		_, err = io.ReadFull(r, buf)
+		if err != nil {
+			return exprOp{}, readError(err)
+		}
+		e.str = string(buf)
 	}
-	return []byte{bcode}, nil
+	return e, nil
 }
 
-func binaryUvarintErr(b []byte) (x uint64, n int, err error) {
-	x, n = binary.Uvarint(b)
-	if n == 0 {
-		err = fmt.Errorf("incomplete uvarint")
-	} else if n < 0 {
-		err = fmt.Errorf("uvarint overflows uint64; %d bytes read", -n)
+func (e *exprOp) WriteTo(w io.Writer) (n int64, err error) {
+	var num []byte
+	if e.code == ExprInt || e.code == ExprName {
+		num = make([]byte, binary.MaxVarintLen64)
+		n := binary.PutUvarint(num, e.int)
+		num = num[:n]
 	}
-	return
-}
-
-func (e *exprOp) UnmarshalBinary(data []byte) error {
-	if len(data) == 0 {
-		return fmt.Errorf("exprOp.UnmarshalBinary: no data")
+	var str string
+	if e.code == ExprName {
+		str = e.str
 	}
-	code := exprOpcode(data[0]
-	if code >= nExprOpcodes {
-		return fmt.Errorf("exprOp.UnmarshalBinary: bad opcode 0x%X", code)
+	b = make([]byte, 1 + len(num) + len(str))
+	b[0] = byte(e.code)
+	copy(b[1:1 + len(num)], num)
+	copy(b[1 + len(num):], str)
+	n, err = w.Write(b)
+	if n > len(b) {
+		panic("exprOp.WriteTo(): invalid Write count")
 	}
-	data = data[1:]
-	if code == ExprInt {
-		if len(data) == 0 {
-			return fmt.Errorf("exprOp.UnmarshalBinary: ExprInt without integer")
-		}
-		x, n, err := binaryUvarintError(data)
-		if err != nil {
-			return fmt.Errorf("exprOp.UnmarshalBinary: ExprInt error: %v", err)
-		}
-		if len(data) != n {
-			return fmt.Erorrf("exprOp.UnmarshalBinary: ExprInt value length mismatch: got %d, want %d", len(data), n)
-		}
-		e.code = code
-		e.int = x
-		return nil
+	if n != len(b) && err == nil {
+		err = io.ErrShortWrite
 	}
-	if code == ExprName {
-		if len(data) == 0 {
-			return fmt.Errorf("exprOp.UnmarshalBinary: ExprName without length or name")
-		}
-		x, n, err := binaryUvarintError(data)
-		if err != nil {
-			return fmt.Errorf("exprOp.UnmarshalBinary: ExprName length error: %v", err)
-		}
-		data = data[n:]
-		if len(data) != x {
-			return fmt.Errorf("exprOp.UnmarshalBinary: ExprName name length mismatch: got %d, want %d", len(data), x)
-		}
-		e.code = code
-		e.str = string(data)
-		return nil
-	}
-	if len(data) != 0 {
-		return fmt.Errorf("exprOp.UnmarshalBinary: unexpected %d extra byte(s) at end", len(data))
-	}
-	e.code = code
-	return nil
+	return n, err
 }
 
 func (e *exprOp) String() string {
@@ -170,7 +176,8 @@ func (e *exprOp) String() string {
 // Expr represents an expression that can be stored in an object file and evaluated by the assembler and linker.
 // Expr implements encoding.BinaryMarshaler and encoding.BinaryUnmarshaler for dictating the format that they appear in object files.
 type Expr struct {
-	ops	[]exprOp
+	ops		[]exprOp
+	finished	bool
 }
 
 func NewExpr() *Expr {
@@ -179,16 +186,12 @@ func NewExpr() *Expr {
 	}
 }
 
-func (e *Expr) finished() bool {
-	return len(e.ops) > 0 && e.ops[len(e.ops) - 1].code == ExprStop
-}
-
 func (e *Expr) Add(code ExprOpcode) error {
-	if e.finished() {
+	if e.finished {
 		return fmt.Errorf("cannot add to finished expression")
 	}
 	if code == ExprInt || code == ExprName {
-		return fmt.Errorf("cannot add %v without argument", code)
+		return fmt.Errorf("cannot add %v using Expr.Add()", code)
 	}
 	e.ops = append(e.ops, exprOp{
 		code:		code,
@@ -197,7 +200,7 @@ func (e *Expr) Add(code ExprOpcode) error {
 }
 
 func (e *Expr) AddInt(n uint64) error {
-	if e.finished() {
+	if e.finished {
 		return fmt.Errorf("cannot add to finished expression")
 	}
 	e.ops = append(e.ops, exprOp{
@@ -207,7 +210,7 @@ func (e *Expr) AddInt(n uint64) error {
 }
 
 func (e *Expr) AddName(name string) error {
-	if e.finished() {
+	if e.finished {
 		return fmt.Errorf("cannot add to finished expression")
 	}
 	if name == "" {
@@ -219,3 +222,42 @@ func (e *Expr) AddName(name string) error {
 		str:			name,
 	})
 }
+
+func (e *Expr) Finish() error {
+	if e.finished {
+		return fmt.Errorf("cannot finish finished expression again")
+	}
+	if len(e.ops) == 0 {
+		return fmt.Errorf("cannot finish empty expression")
+	}
+	e.finished = true
+	return nil
+}
+
+func (e *Expr) Empty() bool {
+	return len(e.ops) == 0
+}
+
+// TODO read
+
+func (e *Expr) WriteTo(w io.Writer) (n int64, err error) {
+	if !e.finished {
+		return 0, fmt.Errorf("cannot write unfinished expression")
+	}
+
+	// first write number of opcodes
+	num = make([]byte, binary.MaxVarintLen64)
+	nn := binary.PutUvarint(num, uint64(len(e.ops)))
+	num = num[:nn]
+
+	// the Write and WriteTo calls here cannot fail according to the documentation for bytes.Buffer
+	buf := new(bytes.Buffer)
+	buf.Write(num)
+	for _, op := range ops {
+		op.WriteTo(buf)
+	}
+
+	return buf.WriteTo(w)
+}
+
+// TODO evaluate
